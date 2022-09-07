0b0bb78bda0b66e159ea04dcb27fc07dded33078
ashutosh4036 <ashutosh.tripathi_cs19@gla.ac.in>
2022-09-07 10:39:35 +0530

    Creating the frontend part

---
+++ frontend/src/components/shared/Navigation/navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import styles from "./navigation.module.css"
+const navigation = () => {
+    const brandStyle = {
+        color: '#fff',
+        textDecoration: 'none',
+        fontWeight: 'bold',
+        fontSize: '22px',
+        display: 'flex',
+        alignItems: 'center',
+    };
+    const logoText = {
+        marginLeft: '10px',
+    };
+  return (
+    <nav className={`${styles.navbar} container`}>
+        <Link style={brandStyle} to="/">
+            <img src="/images/logo.png" alt="logo"/>
+            <span style={logoText}>CodersHouse</span>
+        </Link>
+    </nav>
+  )
+}
+
+export default navigation
