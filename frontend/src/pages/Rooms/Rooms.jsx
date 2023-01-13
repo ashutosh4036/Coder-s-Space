771b63e1b7d3d70b7f5664e6661b0e449af58afa
ashutosh4036 <ashutosh.tripathi_cs19@gla.ac.in>
2023-01-14 00:36:25 +0530

    Created the rooms page

--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import RoomCard from '../../components/RoomCard/RoomCard'
+import styles from './Rooms.module.css'
 const Rooms = () => {
   return (
-    <div>Rooms</div>
+    <>
+      <div className="container">
+        <div className={styles.roomsHeader}>
+          <div className={styles.left}>
+            <span className={styles.heading}>All voice rooms</span>
+            <div className={styles.searchBox}>
+              <img src="/images/search-icon.png" alt ="search"/>
+              <input type="text" className={styles.searchInput}/>
+             </div>
+           </div> 
+          <div className={styles.right}>
+            <button className={styles.startRoomButton}>
+              <img src="/images/add-room-icon.png" alt = "add-room"/>
+              <span> Start a room</span>
+            </button>
+          </div>
+        </div>
+
+        <div className={styles.roomList}>
+          {
+            rooms.map(room =>(
+              <RoomCard/>
+            ))
+          }
+        </div>
+      </div>
+    </>
+    
   )
 }
 
