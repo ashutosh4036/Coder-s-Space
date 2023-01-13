fbc773869282028a567f4ee1a706d6a7e5da3bfd
ashutosh4036 <ashutosh.tripathi_cs19@gla.ac.in>
2023-01-14 00:39:12 +0530

    Fixed some bugs

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../../components/shared/Card/Card';
 import Button from '../../../components/shared/Button/Button';
 import styles from './StepAvatar.module.css';
@@ -13,6 +13,7 @@ const StepAvatar = ({ onNext }) => {
     const { name, avatar } = useSelector((state) => state.activate);
     const [image, setImage] = useState('/images/monkey-avatar.png');
     const [loading, setLoading] = useState(false);
+    const [unMounted, setUnMounted] = useState(false);
     function captureImage(e) {
         const file = e.target.files[0];
         const reader = new FileReader();
@@ -23,21 +24,27 @@ const StepAvatar = ({ onNext }) => {
         };
     }
     async function submit() {
-        if(!name || !avatar) return;
+        if (!name || !avatar) return;
         setLoading(true);
         try {
             const { data } = await activate({ name, avatar });
             if (data.auth) {
-                dispatch(setAuth(data));
+                if (!unMounted) {
+                    dispatch(setAuth(data));
+                }
             }
-            console.log(data);
         } catch (err) {
             console.log(err);
-        }
-        finally{
+        } finally {
             setLoading(false);
         }
     }
+
+    useEffect(() => {
+        return () => {
+            setUnMounted(true);
+        };
+    }, []);
 
     if(loading) return <Loader message = "Activation in progress..."/>
     return (
